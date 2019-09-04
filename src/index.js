083df9a09aa1455fcb4e3480b622a03fb0599c38
Lee EC <lyxuncle@gmail.com>
2019-09-04 14:37:51 +0800

    feat: 更新配置功能、本地缓存

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ try {
 
 const handleReport = () => {
   Report.init({})
-  Report.processPV()
+  Report.processPV(22)
 }
 
 if (typeof window.XView === 'undefined') {
--- src/js/controlinit.js
+++ src/js/controlinit.js
@@ -6,20 +6,32 @@
 import dat from '@o2team/ambient-dat.gui'
 import {
   O2_AMBIENT_MAIN,
-  O2_AMBIENT_CONFIG
+  O2_AMBIENT_CONFIG,
+  O2_AMBIENT_CLASSNAME
 } from './utils/const'
 import Controller from './utils/controller'
 import { getParameterByName } from './utils/util'
+import processLocalConfig from './utils/processLocalConfig'
+
+import configKeys from './configs/keys'
 
 /* eslint-disable no-unused-vars */
 const isLoop = getParameterByName('loop')
+const configKeyVal = getParameterByName('configKey')
+const configKey = configKeys[configKeyVal] || configKeys['default']
+
+const loadData = {
+  "默认": {
+    "0": {...window[O2_AMBIENT_CONFIG]}
+  }
+}
+const allLoadData = processLocalConfig({ configKey, guiName: O2_AMBIENT_CLASSNAME, loadData })
 
 let controlInit = () => {
   // 非必要配置字段（仅用于展示，如背景颜色、启动/暂停）
   class OtherConfig {
     constructor () {
       this.isPaly = true
-      this.message = '弹幕效果'
       this.backgroundColor = '#333'
       this.play = () => {
         if (this.isPaly) {
@@ -46,12 +58,19 @@ let controlInit = () => {
     initBaseGUI () {
       const config = this.config
       const otherConfig = this.otherConfig
-      const gui = new dat.GUI()
+      const gui = new dat.GUI({
+        name: O2_AMBIENT_CLASSNAME,
+        preset: configKey,
+        load: {
+          "remembered": { ...allLoadData.remembered }
+        }
+      })
+      gui.useLocalStorage = true
+      gui.remember(config)
       gui.addCallbackFunc(this.resetCanvas.bind(this))
 
-      gui.add(otherConfig, 'message').name('配置面板')
       gui.add(otherConfig, 'play').name('播放 / 暂停')
-      gui.addColor(otherConfig, 'backgroundColor').name('背景颜色').onFinishChange(val => {
+      gui.addColor(otherConfig, 'backgroundColor').name('背景颜色（仅预览）').onFinishChange(val => {
         this.setBackgroundColor(val)
       })
 
